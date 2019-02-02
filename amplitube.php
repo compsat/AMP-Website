42bae0cedf8e3cebdbe0cfd19233d41ab29aa389
Miguel Habana <mighabana@gmail.com>
2019-02-02 21:12:17 +0800

    WIP working article transitions and content for Amplitube

--- amplitube.php
+++ amplitube.php
@@ -14,7 +14,7 @@
         <?php createHeader('Amplitube'); ?>
         <div class='amplitube'>
             <div class='wrapper'>
-                <img id='tube' src='/../images/icons/tube_logo.png'>
+                <a href='/amplitube.php'><img id='tube' src='/../images/icons/tube_logo.png'></a>
             </div>
             <div class='wrapper'>
                 <h1><?php echo get($meta, 'about-heading'); ?></h1>
@@ -26,16 +26,14 @@
             <div class='wrapper'>
                 <div class='card-wrapper'><?php
                 $articles = json('articles');
-                paragraph(gettype($articles));
-                $id = 0;
                 foreach ($articles as $article) { 
                     $image = $article['image'];
                     if ($image) $image = "src='$image'"; ?>
-                    <div class="card" index=<?php echo $id++; ?>>
-                        <a href='/article.php'><h5><?php echo $article['title']; ?></h5></a>
+                    <div class="card" index=<?php echo $article['id']; ?>>
+                        <a href='/article.php?article_id=<?php echo $article['id'] ?>'><h5><?php echo $article['title']; ?></h5></a>
                         <?php echo paragraph($article['date_published']); ?>
                         <?php echo paragraph($article['credits']); ?>
-                        <a href='/article.php'><img <?php echo $image; ?><a>
+                        <a href='/article.php?article_id=<?php echo $article['id'] ?>'><img <?php echo $image; ?><a>
                     </div> 
                 <?php } unset($articles); ?>
                 </div>
--- article.php
+++ article.php
@@ -13,72 +13,28 @@
     <body id='holy-grail'>
         <div class='article'>
             <div class='wrapper'>
-                <img id='tube' src='/../images/icons/tube_logo.png'>
+                <a href='/amplitube.php'><img id='tube' src='/../images/icons/tube_logo.png'></a>
             </div>
             <div class='wrapper'>
-                <div class='main-content'>
-                    <h4>OPM - WHAT IS IT AND WHY IS IT DEAD?</h4>
+                <div class='main-content'><?php
+                    $articles = json('articles');
+                    $article = NULL;
+                    foreach($articles as $a) {
+                        if($a['id'] == $_GET['article_id']){
+                            $article = $a;
+                            break;
+                        };
+                    };?>
+                    <h4><?php echo $article['title'] ?></h4>
                     <div class = subheading>
-                        <p>November 13, 2018</p>
+                        <p><?php echo $article['date_published']?></p>
                         <p>|</p>
-                        <p>Sofia Guanzon</p>
+                        <p><?php echo $article['credits']?></p>
                     </div>
                     <div class='main-image'>
-                        <img src ='../images/articles/opm.png'>
+                        <img src ='..<?php echo $article['image']?>'>
                     </div>
-                    <p>You’re in the car; switch the radio to a random local station and an American pop song croons about unrequited love. You open your phone, go on Twitter, and scroll through the masses of 280 character bits of people’s lives, filtering through the hot takes and glazing over their experiences. You’re half-interested but really, you’re just doing this because you’ve been conditioned to use your phone during idle time. Then a tweet pops out in particular and suddenly you’re a witness to the death of an entire art form. “OPM IS NOT DEAD. There are so many new artists you should listen to. It never died!” a tweet reads, black characters glowing against the stark white screen. You hear the American pop song playing in the background and think: maybe it is?</p>
-                    <br>
-                    <p>OPM or Original Pilipino, Pinoy or Philippine music is a movement, genre, nuisance—however you’d like to define it, that has been a topic of much debate over the past few years. With the rise of this notion that ‘OPM is not dead’ on the internet, it sparks even more discourse on the implication that it ever ‘died’.</p>
-                    <br>
-                    <p><b>Etymology and origins of OPM</b></p>
-                    <br>
-                    <p>Historically speaking, the term OPM was said to have been coined in the late 1970s by Danny Javier of APO Hiking Society, preceded by the term “Manila Sound” which was also popularized in the seventies. The two movements coincided with one another as the musical landscape began to shift from public friendly Americana disco mania, characterized by artists such as Hotdog, VST & Co, and The Boyfriends to a more Filipinized take on pop rock music with Tagalog being the main language of expression. Arguably, Manila Sound was already “OPM”, however, OPM was a more inclusive term that contrasted the Manila-centric limitations of calling Filipino music just Manila Sound. Juxtaposed by the cultural context of Martial Law in the seventies, there seemed to be a need to Filipinize our art as a way to claim our national identity during a time when the strength of the Filipino and Philippine society was being destroyed by a dictator. These distinct terminological turning points in Philippine music history established that the music we were creating was never simply a Filipino take on Western music  — it was emerging to be something we were doing on our own.</p>
-                    <br>
-                    <p>From the seventies to the eighties came the rise of ballad, rap, and rock music making their way to the public sphere through the radio. The eighties and nineties bore witness to the rise of subgenres such as alternative rock, headed by bands like the Eraserheads, Parokya ni Edgar and Rivermaya; simultaneously, soloists like Gary Valenciano and Regine Velasquez were serenading the country with their love ballads. While on the opposite side of the spectrum, Francis Magalona and Andrew E. were trailblazers of the Filipino rap scene in terms of bringing the previously lesser known subgenre into the mainstream. Considering the definition and purpose of the term, along with the musical landscape of the time, these artists and the music they made are undoubtedly OPM.</p>
-                    <br>
-                    <p>It is only now; that OPM’s validity as a collective term, movement, or genre is being questioned. Previously, there was no set sound for OPM, there hadn’t been such growth and development in Filipino music prior to the eighties and nineties, when the ‘genre’ was fully fleshed out. The way these artists made music led them to define a standard and style of OPM, and in turn popularized the sounds that we most commonly associate with the genre today. From the seventies to the nineties and even the early 2000s, there seemed to be shared sentiment that all Filipino music—popular and independent — was OPM. However, the industry and the way music is promoted have changed. These transformations have ushered in a proposition of whether the labels we have created are still applicable to the current state of our music scene and if the artists can still prescribe to it.</p>
-                    <br>
-                    <p><b>Concretizing ‘OPM’</b></p>
-                    <br>
-                    <p>The term ‘Original Pilipino Music’ already poses a terminological problem that lends itself to be dissected in ways that divide creators and consumers alike. Who has the monopoly over what is defined to be original? What does ‘Pilipino’ entail—the language, the artist, the country? What kind of music is encompassed here and can new artists who don’t prescribe to the traditional sound of OPM be part of it? Having the term be in English already adds another dimension to the problem of imposing a foreign tongue on what is meant to be an umbrella term for music made by Filipinos, for Filipinos. Its dichotomy of being either genre or movement is also always in question. Defining OPM as a genre implies that there is a certain criterion that must be accomplished for it to be considered as part of such. Defining it as a movement implies that there is an agreed upon cause or goal being set into motion through the creation of this art. This plurality of interpretations is a common phenomenon ingrained in our culture. Our national identity and what it means to be Filipino is still being debated over today, and this lack of cohesion trickles its way down to defining our subcultures and art.</p>
-                    <br>
-                    <p>There are those who insist that the term has grown obsolete. Some artists say they feel constricted by the term, that it specializes Filipino music to be a certain way and hinders them from progressing artistically. One of the most common arguments for abolishing the term altogether stems from comparison with other countries not having specialized terms for music of their own. It’s simply called their music. If music is made by a Filipino in the Philippines, surely it shouldn’t be necessary to specifically call it ‘original’ or ‘Pilipino’ at all.</p>
-                    <br>
-                    <p>In spite of this, there are also those who don’t necessarily think that the term poses any extreme limitations on artists. After all, it is a very Pinoy quality to use one specific term to collectively classify, like how we refer to Colgate as the general term for toothpaste when it’s a specific brand. In that sense, OPM is a familiar cultural concept that bridges music to the masses. Artists and fans can contest the label of OPM as much as they want yet at the end of the day, Filipino music is pooled altogether under this term because it has been convenient and apt for us until this point.</p>
-                    <br>
-                    <p>As for the question of who can be part of OPM: if any Filipino can be considered part of it, even those who grew up abroad—it’s all rooted in our definition of who is Filipino in the first place. Like most things in our culture, it’s all a matter of our own experiences. Filipino artists who make music actively keep their national identity alive and intact through their art and contribute to creating this national heritage. That is an act of being Filipino in itself.<p>
-                    <br>
-                    <p><b>Here lies OPM</b></p>
-                    <br>
-                    <p>So how does one declare an ultimatum on the lifespan of a people’s music?</p>
-                    <br>
-                    <p>OPM has been subjected to a sentence of antiquation ever since the previous sounds associated with it had fallen out of popularity. Although, the problem does not solely lie in the term itself. Mediums evolve, and the processes and patterns that come with being an artist must adapt with the times. The main difference with how artists in the past were able to achieve success in the market compared to today lies in the methods through which they put out their music. Artists who made it big in the past were invested in by large record labels who provided them with the resources to fully develop their vision, the sound they wanted to create, and the narratives they wanted to set across. Commercial record labels of today typically sign solo artists who are either already established actors or starlets who have already been launched into the industry. There is also a risk of sacrificing their own personal vision in pursuit of reaching nationwide success. This led to a shift in preference for artists to create music on their own terms.</p>
-                    <br>
-                    <p>With this ‘death’ of OPM, there are specific artists who gain popularity and give a face to modern Filipino music. Usually, you’d see a comment or two on their Youtube videos, where people would be relieved to see that OPM really isn’t dead. IV of Spades, Ben & Ben, and Reese are just some of the artists who are commonly named as saviors of OPM through statements defending that it ever died. A commonality between these artists is that their unorthodox sound doesn’t prescribe to the sound that the Eraserheads pioneered or that APO Hiking Society popularized. It’s new, it’s current, and it’s groundbreaking to those who have never been directly involved with the music community to witness the blossoming of these new artists. Just as anything in the music scene, it’s all about the fans—and they have spoken. Whether you agree with the sentiment that there are certain artists we can pin as messiahs of our music or not, that’s not the message. It’s more of a testament to the response an artist can get today, if their music was more accessible. One artist’s success only elevates the community as a whole anyway. Maybe if every artist was invested in commercially and promoted better, the notion that OPM ever died would have never been a topic of discussion.</p>
-                    <br>
-                    <p>The independent music scene is where most artists who don’t abide with the current system of the industry operate in today. The term ‘independent music’ or ‘indie music’ is constantly misused as a genre as ‘indie’ may also refer to an artist not prescribing to the previously defined musical genres. Although in that way, that may be a style or genre in itself. Terminological complications and categorical confusion aside, artists are now choosing to make music in a more niche way that does not appeal to the general public - making their music independently gives them the avenue to do so.</p>
-                    <br>
-                    <p><b>A systemic change</b></p>
-                    <br>
-                    <p>Globalization and the internet have completely revolutionized the way we consume music. Internet culture has cultivated the rise of producers and beatmakers – artists who can now create entire albums and distribute them right from their bedroom without the additional involvement of a company to be the channel between them and the industry. Music streaming is now the most popular form of accessing and listening to music through websites such as Soundcloud and Spotify. In comparison to how artists were marketed in the past, this new system is not the most inclusive approach to accelerate one’s success here in the Philippines.</p>
-                    <br>
-                    <p>Perhaps this is where OPM’s death begins—; with the metamorphosis of how we distribute and consume music.</p>
-                    <br>
-                    <p>Here in the Philippines, the radio and TV are still the most effective mediums through which new artists are given exposure and can assimilate into mainstream music. By giving artists the opportunity to be heard and be accessed to on a larger scale, this gives consumers the space to simply be fans. Exposure through television shows, promoting on radio or even mall shows may be an outdated way to reach out to their audience but these are the methods that resonate with Filipinos. The internet can only do so much and only reach a certain population. These platform changes have led to the idea that OPM ever died since we didn’t really hear Filipino artists on the radio as much as we heard Western artists.</p>
-                    <br>
-                    <p><b>A eulogy to OPM</b></p>
-                    <br>
-                    <p>Technicalities aside, OPM actually might be dead. Its previous incarnations, that is, have died and in turn have paved the way for new subcultures and new techniques of producing music. In the years that people have declared it to not be dead, it seems to be more of an issue of a popular sound to hit plateau rather than it actually ‘dying’.  With the spotlight shifting focus to more famous international pop artists who have the means to market themselves extensively, especially online, our homegrown acts seem to have grown within the smaller independent music community instead. Gig culture—another subculture stemming from the independent music scene, brings us more intimate and personal ways to fully immerse ourselves into the scene. In contrast to colossal concerts in arenas, gigs bring the artists to the forefront in a less formal setting. Usually organized by a small group of people, gigs are also the main way independent artists get their name out there. Any gig, from Katipunan to Makati, will assure you that our music is very much pulsing with life, the artists and members of the independent music community have just taken matters into their own hands.</p>
-                    <br>
-                    <p><b>A call to action</b></p>
-                    <br>
-                    <p>We can’t antagonize people who thought OPM was dead when all they heard on the radio was Ed Sheeran. Filipinos love what’s popular, what’s cool, what’s trending and what is popular, because what’s cool and trending is dictated by the media giants who control what the average citizens hear on their way to work. We also can’t villainize the artists who feel offended or limited by the term. That’s their personal experience and the best we can do as a nation, as a music community, is to listen to their stories.</p>
-                    <br>
-                    <p>So the next time you see another eulogy to OPM online and you find yourself mourning as well, let it rest in peace. To some people, it never died. To others, it did, and we are now in an era of OPM Renaissance. Music is all about your personal experience and, terminological discourse aside, as long as a Filipino has something to say through a song, all other elements are secondary. At the heart of the industry is being a fan, enjoying the stories artists have to share, and talking about these opinions and experiences. OPM is about your personal experience with Filipino music; and as to whether this term benefits our music in the long run—I hand the pen over to you now for you to contribute to the discussion.</p>
-                    <br>
-                    <p>Okay, enough talking. Turn the music back on.</p>
-                    <br>
-                    <p><i>The views, thoughts, and opinions expressed in this article belong to the author, and do not reflect the thoughts and opinions of the Ateneo Musicians' Pool as a whole.</i></p>
+                    <?php echo $article['content']?>
                 </div>
             </div>
         </div>
